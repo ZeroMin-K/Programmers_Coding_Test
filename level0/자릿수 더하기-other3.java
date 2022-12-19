5e5dbed6839033536ffef8cfaa4e1ebb8f27a89c
ZeroMin-K <ymkim9510@gmail.com>
2022-12-19 12:27:29 +0900

    프로그래머스 - 코딩테스트 문제풀이 - level 0

---
+++ level0/자릿수 더하기-other3.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+   public int solution(int n) {
+        String[] split = String.valueOf(n).split("");
+        return Arrays.stream(split).mapToInt(Integer::parseInt).sum();
+    }
+}
